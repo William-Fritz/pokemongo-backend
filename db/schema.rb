9def133bc173719b3abed95a9b9eeedbbd88ba11
william-fritz <splizert@gmail.com>
2020-03-06 12:55:35 +0700

    rails db migrate:

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_053823) do
+ActiveRecord::Schema.define(version: 2020_03_06_055334) do
+
+  create_table "inventories", force: :cascade do |t|
+    t.string "item_name"
+    t.integer "quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "players", force: :cascade do |t|
     t.string "name"
     t.integer "currency"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "pokemon_eggs", force: :cascade do |t|
+    t.string "type"
+    t.integer "distance_traveled"
+    t.integer "distance_needed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "pokemons", force: :cascade do |t|
+    t.string "name"
+    t.integer "lvl"
+    t.integer "exp"
+    t.string "status"
+    t.string "type1"
+    t.string "type2"
+    t.string "move1"
+    t.string "move2"
+    t.boolean "item"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "shops", force: :cascade do |t|
+    t.string "item_name"
+    t.integer "sell_price"
+    t.integer "buy_price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
