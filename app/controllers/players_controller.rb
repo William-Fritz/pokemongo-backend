d035fa922e8f8332a639821a41c62f96c04e3597
william-fritz <splizert@gmail.com>
2020-03-06 21:40:20 +0700

    edit player name api done

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -19,6 +19,21 @@ class PlayersController < ApplicationController
     @player = Player.find(params[:id])
   end
 
+  def edit
+    @player = Player.find(params[:id])
+  end
+
+  def update
+    @player = Player.find(params[:id])
+
+    if @player.update(player_params)
+      redirect_to @player
+    else
+      render 'edit'
+    end
+  end
+
+
   private
   def player_params
     params.require(:player).permit(:name, :currency)
