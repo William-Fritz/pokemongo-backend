3d59e839a152b63ade0b6e6c5ec62b1132d555f0
william-fritz <splizert@gmail.com>
2020-03-06 17:37:22 +0700

    player create, index, and show done

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,27 @@
 class PlayersController < ApplicationController
+  skip_forgery_protection
+
+  def new
+  end
+  
+  def create
+    @player = Player.new(player_params)
+
+    @player.save
+    redirect_to @player
+  end
+
+  def index
+    @players = Player.all
+  end
+
+  def show
+    @player = Player.find(params[:id])
+  end
+
+  private
+  def player_params
+    params.require(:player).permit(:name, :currency)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :players, :pokemons, :pokemon_eggs, :shops, :inventories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
